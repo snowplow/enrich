snowplow/snowplow

Scala Common Enrich: bump to 0.25.0 (closes #3089)

3-enrich/scala-common-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.common/enrichments/miscEnrichmentSpecs.scala
@@ -37,7 +37,7 @@ class EtlVersionSpec extends MutSpecification {
 
   "The ETL version" should {
     "be successfully returned" in {
-      MiscEnrichments.etlVersion("hadoop-x.x.x") must_== "hadoop-x.x.x-common-0.24.0"
+      MiscEnrichments.etlVersion("hadoop-x.x.x") must_== "hadoop-x.x.x-common-0.25.0"
     }
   }
 }

snowplow/enrich

modules/common/src/test/scala/com.snowplowanalytics.snowplow.enrich.common/enrichments/miscEnrichmentSpecs.scala
@@ -37,7 +37,7 @@ class EtlVersionSpec extends MutSpecification {
 
   "The ETL version" should {
     "be successfully returned" in {
-      MiscEnrichments.etlVersion("hadoop-x.x.x") must_== "hadoop-x.x.x-common-0.24.1"
+      MiscEnrichments.etlVersion("spark-x.x.x") must_== "spark-x.x.x-common-0.25.0"
     }
   }
 }
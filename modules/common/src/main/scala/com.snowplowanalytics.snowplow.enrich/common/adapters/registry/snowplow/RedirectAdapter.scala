drewsonne/snowplow

Scala Common Enrich: apply automated code formatting (closes #3532)

3-enrich/scala-common-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/adapters/registry/snowplow/RedirectAdapter.scala
@@ -28,11 +28,7 @@ import com.fasterxml.jackson.databind.JsonNode
 import scala.collection.JavaConversions._
 
 // Iglu
-import iglu.client.{
-  SchemaCriterion,
-  Resolver,
-  SchemaKey
-}
+import iglu.client.{SchemaCriterion, Resolver, SchemaKey}
 import iglu.client.validation.ValidatableJsonMethods._
 
 // Scalaz
@@ -46,7 +42,7 @@ import org.json4s.jackson.JsonMethods._
 
 // This project
 import loaders.CollectorPayload
-import utils.{JsonUtils => JU}
+import utils.{JsonUtils       => JU}
 import utils.{ConversionUtils => CU}
 
 /**
@@ -89,46 +85,43 @@ object RedirectAdapter extends Adapter {
   def toRawEvents(payload: CollectorPayload)(implicit resolver: Resolver): ValidatedRawEvents = {
 
     val originalParams = toMap(payload.querystring)
-    if (originalParams.isEmpty) {
+    if(originalParams.isEmpty) {
       "Querystring is empty: cannot be a valid URI redirect".failNel
     } else {
       originalParams.get("u") match {
-        case None    => "Querystring does not contain u parameter: not a valid URI redirect".failNel
+        case None => "Querystring does not contain u parameter: not a valid URI redirect".failNel
         case Some(u) => {
 
           val json = buildUriRedirect(u)
           val newParams =
-            if (originalParams.contains("e")) {
+            if(originalParams.contains("e")) {
               // Already have an event so add the URI redirect as a context (more fiddly)
               def newCo = Map("co" -> compact(toContexts(json))).successNel
               (originalParams.get("cx"), originalParams.get("co")) match {
-                case (None, None)                 => newCo
-                case (None, Some(co)) if co == "" => newCo
-                case (None, Some(co))             => addToExistingCo(json, co)
-                                                       .map(str => Map("co" -> str))
-                case (Some(cx), _)                => addToExistingCx(json, cx)
-                                                       .map(str => Map("cx" -> str))
+                case (None,     None)                 => newCo
+                case (None,     Some(co)) if co == "" => newCo
+                case (None,     Some(co))             => addToExistingCo(json, co).map(str => Map("co" -> str))
+                case (Some(cx), _)                    => addToExistingCx(json, cx).map(str => Map("cx" -> str))
               }
             } else {
-              // Add URI redirect as an unstructured event 
-              Map("e"     -> "ue",
-                  "ue_pr" -> compact(toUnstructEvent(json))
-                ).successNel
+              // Add URI redirect as an unstructured event
+              Map("e" -> "ue", "ue_pr" -> compact(toUnstructEvent(json))).successNel
             }
 
           val fixedParams = Map(
             "tv" -> TrackerVersion,
             "p"  -> originalParams.getOrElse("p", TrackerPlatform) // Required field
-            )
+          )
 
           for {
             np <- newParams
-            ev = NonEmptyList(RawEvent(
-              api          = payload.api,
-              parameters   = (originalParams - "u") ++ np ++ fixedParams,
-              contentType  = payload.contentType,
-              source       = payload.source,
-              context      = payload.context
+            ev = NonEmptyList(
+              RawEvent(
+                api         = payload.api,
+                parameters  = (originalParams - "u") ++ np ++ fixedParams,
+                contentType = payload.contentType,
+                source      = payload.source,
+                context     = payload.context
               ))
           } yield ev
         }
@@ -146,9 +139,9 @@ object RedirectAdapter extends Adapter {
    */
   private def buildUriRedirect(uri: String): JValue =
     ("schema" -> SchemaUris.UriRedirect) ~
-    ("data" -> (
-      ("uri" -> uri)
-    ))
+      ("data" -> (
+        ("uri" -> uri)
+      ))
 
   /**
    * Adds a context to an existing non-Base64-encoded
@@ -167,9 +160,9 @@ object RedirectAdapter extends Adapter {
    */
   private def addToExistingCo(newContext: JValue, existing: String): Validated[String] =
     for {
-      node   <- JU.extractJson("co|cx", existing).toValidationNel: Validated[JsonNode]
-      jvalue  = fromJsonNode(node)
-      merged  = jvalue merge render("data" -> List(newContext))
+      node <- JU.extractJson("co|cx", existing).toValidationNel: Validated[JsonNode]
+      jvalue = fromJsonNode(node)
+      merged = jvalue merge render("data" -> List(newContext))
     } yield compact(merged)
 
   /**
@@ -191,7 +184,7 @@ object RedirectAdapter extends Adapter {
     for {
       decoded <- CU.decodeBase64Url("cx", existing).toValidationNel: Validated[String]
       added   <- addToExistingCo(newContext, decoded)
-      recoded  = CU.encodeBase64Url(added)
+      recoded = CU.encodeBase64Url(added)
     } yield recoded
 
 }

3-enrich/scala-common-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/enrichments/registry/IpLookupsEnrichment.scala
@@ -34,33 +34,27 @@ import Scalaz._
 import org.json4s.JValue
 
 // Iglu
-import iglu.client.{
-  SchemaCriterion,
-  SchemaKey
-}
+import iglu.client.{SchemaCriterion, SchemaKey}
 import iglu.client.validation.ProcessingMessageMethods._
 
 // Scala MaxMind GeoIP
-import maxmind.iplookups.{
-  IpLookups,
-  IpLookupResult
-}
+import maxmind.iplookups.{IpLookups, IpLookupResult}
 
 // This project
 import common.utils.ConversionUtils
 import utils.ScalazJson4sUtils
 
 /**
-* Companion object. Lets us create an IpLookupsEnrichment
-* instance from a JValue.
-*/
+ * Companion object. Lets us create an IpLookupsEnrichment
+ * instance from a JValue.
+ */
 object IpLookupsEnrichment extends ParseableEnrichment {
 
   val supportedSchema = SchemaCriterion("com.snowplowanalytics.snowplow", "ip_lookups", "jsonschema", 1, 0)
 
   /**
    * Creates an IpLookupsEnrichment instance from a JValue.
-   * 
+   *
    * @param config The ip_lookups enrichment JSON
    * @param schemaKey The SchemaKey provided for the enrichment
    *        Must be a supported SchemaKey for this enrichment
@@ -68,13 +62,11 @@ object IpLookupsEnrichment extends ParseableEnrichment {
    *        Enabled for tests
    * @return a configured IpLookupsEnrichment instance
    */
-  def parse(config: JValue, schemaKey: SchemaKey, localMode: Boolean): ValidatedNelMessage[IpLookupsEnrichment] = {
-
-    isParseable(config, schemaKey).flatMap( conf => {
+  def parse(config: JValue, schemaKey: SchemaKey, localMode: Boolean): ValidatedNelMessage[IpLookupsEnrichment] =
+    isParseable(config, schemaKey).flatMap(conf => {
       def db(name: String) = getArgumentFromName(conf, name).sequenceU
-      (db("geo") |@| db("isp") |@| db("organization") |@| db("domain") |@| db("netspeed")) { IpLookupsEnrichment(_,_,_,_,_,localMode) }
+      (db("geo") |@| db("isp") |@| db("organization") |@| db("domain") |@| db("netspeed")) { IpLookupsEnrichment(_, _, _, _, _, localMode) }
     })
-  }
 
   /**
    * Creates the (URI, String) tuple arguments
@@ -87,9 +79,8 @@ object IpLookupsEnrichment extends ParseableEnrichment {
    *         Some(Failure) if its URI is invalid,
    *         Some(Success) if it is found
    */
-  private def getArgumentFromName(conf: JValue, name: String): Option[ValidatedNelMessage[(String, URI, String)]] = {
-
-    if (ScalazJson4sUtils.fieldExists(conf, "parameters", name)) {
+  private def getArgumentFromName(conf: JValue, name: String): Option[ValidatedNelMessage[(String, URI, String)]] =
+    if(ScalazJson4sUtils.fieldExists(conf, "parameters", name)) {
       val uri = ScalazJson4sUtils.extract[String](conf, "parameters", name, "uri")
       val db  = ScalazJson4sUtils.extract[String](conf, "parameters", name, "database")
 
@@ -101,7 +92,6 @@ object IpLookupsEnrichment extends ParseableEnrichment {
       }.flatMap(x => x).some
 
     } else None
-  }
 
   /**
    * Convert the Maxmind file from a
@@ -114,10 +104,13 @@ object IpLookupsEnrichment extends ParseableEnrichment {
    * @return a Validation-boxed URI
    */
   private def getMaxmindUri(uri: String, database: String): ValidatedMessage[URI] =
-    ConversionUtils.stringToUri(uri + "/" + database).flatMap(_ match {
-      case Some(u) => u.success
-      case None => "URI to MaxMind file must be provided".fail
-      }).toProcessingMessage
+    ConversionUtils
+      .stringToUri(uri + "/" + database)
+      .flatMap(_ match {
+        case Some(u) => u.success
+        case None    => "URI to MaxMind file must be provided".fail
+      })
+      .toProcessingMessage
 }
 
 /**
@@ -137,31 +130,32 @@ object IpLookupsEnrichment extends ParseableEnrichment {
  * @param netspeedTuple (Full URI to the netspeed
  *        lookup MaxMind data file, database name)
  * @param localMode Whether to use the local
- *        MaxMind data file. Enabled for tests. 
+ *        MaxMind data file. Enabled for tests.
  */
 case class IpLookupsEnrichment(
-  geoTuple: Option[(String, URI, String)],
-  ispTuple: Option[(String, URI, String)],
-  orgTuple: Option[(String, URI, String)],
-  domainTuple: Option[(String, URI, String)],
+  geoTuple: Option[(String,      URI, String)],
+  ispTuple: Option[(String,      URI, String)],
+  orgTuple: Option[(String,      URI, String)],
+  domainTuple: Option[(String,   URI, String)],
   netspeedTuple: Option[(String, URI, String)],
   localMode: Boolean
-  ) extends Enrichment {
+) extends Enrichment {
 
   val version = new DefaultArtifactVersion("0.1.0")
 
   private type FinalPath = String
-  private type DbEntry = Option[(Option[URI], FinalPath)]
+  private type DbEntry   = Option[(Option[URI], FinalPath)]
 
   // Construct a Tuple5 of all the IP Lookup databases
   private val dbs: Tuple5[DbEntry, DbEntry, DbEntry, DbEntry, DbEntry] = {
 
-    def db(dbPath: Option[(String, URI, String)]): DbEntry = dbPath.map { case (name, uri, file) =>
-      if (localMode) {
-        (None, getClass.getResource(file).toURI.getPath)
-      } else {
-        (Some(uri), "./ip_" + name)
-      }
+    def db(dbPath: Option[(String, URI, String)]): DbEntry = dbPath.map {
+      case (name, uri, file) =>
+        if(localMode) {
+          (None, getClass.getResource(file).toURI.getPath)
+        } else {
+          (Some(uri), "./ip_" + name)
+        }
     }
 
     (db(geoTuple), db(ispTuple), db(orgTuple), db(domainTuple), db(netspeedTuple))
@@ -202,12 +196,10 @@ case class IpLookupsEnrichment(
    *         boxed in a Scalaz Validation
    */
   // TODO: can we move the IpGeo to an implicit?
-  def extractIpInformation(ip: String): Validation[String, IpLookupResult] = {
-
+  def extractIpInformation(ip: String): Validation[String, IpLookupResult] =
     try {
       ipLookups.performLookups(ip).success
     } catch {
       case NonFatal(e) => "Could not extract geo-location from IP address [%s]: [%s]".format(ip, e).fail
     }
-  }
 }

snowplow/enrich

modules/common/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/adapters/registry/snowplow/RedirectAdapter.scala
@@ -22,10 +22,7 @@ package snowplow
 import com.fasterxml.jackson.databind.JsonNode
 
 // Iglu
-import iglu.client.{
-  Resolver,
-  SchemaKey
-}
+import iglu.client.{Resolver, SchemaCriterion, SchemaKey}
 import iglu.client.validation.ValidatableJsonMethods._
 
 // Scalaz
@@ -39,7 +36,7 @@ import org.json4s.jackson.JsonMethods._
 
 // This project
 import loaders.CollectorPayload
-import utils.{JsonUtils => JU}
+import utils.{JsonUtils       => JU}
 import utils.{ConversionUtils => CU}
 
 /**
@@ -86,40 +83,38 @@ object RedirectAdapter extends Adapter {
       "Querystring is empty: cannot be a valid URI redirect".failNel
     } else {
       originalParams.get("u") match {
-        case None    => "Querystring does not contain u parameter: not a valid URI redirect".failNel
+        case None => "Querystring does not contain u parameter: not a valid URI redirect".failNel
         case Some(u) =>
-
           val json = buildUriRedirect(u)
           val newParams =
             if (originalParams.contains("e")) {
               // Already have an event so add the URI redirect as a context (more fiddly)
               def newCo = Map("co" -> compact(toContext(json))).successNel
               (originalParams.get("cx"), originalParams.get("co")) match {
-                case (None, None)                 => newCo
-                case (None, Some(co)) if co == "" => newCo
-                case (None, Some(co))             => addToExistingCo(json, co)
-                                                       .map(str => Map("co" -> str))
-                case (Some(cx), _)                => addToExistingCx(json, cx)
-                                                       .map(str => Map("cx" -> str))
+                case (None,     None)                 => newCo
+                case (None,     Some(co)) if co == "" => newCo
+                case (None,     Some(co))             => addToExistingCo(json, co).map(str => Map("co" -> str))
+                case (Some(cx), _)                    => addToExistingCx(json, cx).map(str => Map("cx" -> str))
               }
             } else {
-              // Add URI redirect as an unstructured event 
+              // Add URI redirect as an unstructured event
               Map("e" -> "ue", "ue_pr" -> compact(toUnstructEvent(json))).successNel
             }
 
           val fixedParams = Map(
             "tv" -> TrackerVersion,
             "p"  -> originalParams.getOrElse("p", TrackerPlatform) // Required field
-            )
+          )
 
           for {
             np <- newParams
-            ev = NonEmptyList(RawEvent(
-              api          = payload.api,
-              parameters   = (originalParams - "u") ++ np ++ fixedParams,
-              contentType  = payload.contentType,
-              source       = payload.source,
-              context      = payload.context
+            ev = NonEmptyList(
+              RawEvent(
+                api         = payload.api,
+                parameters  = (originalParams - "u") ++ np ++ fixedParams,
+                contentType = payload.contentType,
+                source      = payload.source,
+                context     = payload.context
               ))
           } yield ev
       }
@@ -136,9 +131,9 @@ object RedirectAdapter extends Adapter {
    */
   private def buildUriRedirect(uri: String): JValue =
     ("schema" -> SchemaUris.UriRedirect) ~
-    ("data" -> (
-      ("uri" -> uri)
-    ))
+      ("data" -> (
+        ("uri" -> uri)
+      ))
 
   /**
    * Adds a context to an existing non-Base64-encoded
@@ -157,9 +152,9 @@ object RedirectAdapter extends Adapter {
    */
   private def addToExistingCo(newContext: JValue, existing: String): Validated[String] =
     for {
-      node   <- JU.extractJson("co|cx", existing).toValidationNel: Validated[JsonNode]
-      jvalue  = fromJsonNode(node)
-      merged  = jvalue merge render("data" -> List(newContext))
+      node <- JU.extractJson("co|cx", existing).toValidationNel: Validated[JsonNode]
+      jvalue = fromJsonNode(node)
+      merged = jvalue merge render("data" -> List(newContext))
     } yield compact(merged)
 
   /**
@@ -181,7 +176,7 @@ object RedirectAdapter extends Adapter {
     for {
       decoded <- CU.decodeBase64Url("cx", existing).toValidationNel: Validated[String]
       added   <- addToExistingCo(newContext, decoded)
-      recoded  = CU.encodeBase64Url(added)
+      recoded = CU.encodeBase64Url(added)
     } yield recoded
 
 }

modules/common/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/enrichments/registry/IpLookupsEnrichment.scala
@@ -34,33 +34,27 @@ import Scalaz._
 import org.json4s.JValue
 
 // Iglu
-import iglu.client.{
-  SchemaCriterion,
-  SchemaKey
-}
+import iglu.client.{SchemaCriterion, SchemaKey}
 import iglu.client.validation.ProcessingMessageMethods._
 
 // Scala MaxMind GeoIP
-import maxmind.iplookups.{
-  IpLookups,
-  IpLookupResult
-}
+import maxmind.iplookups.{IpLookupResult, IpLookups}
 
 // This project
 import common.utils.ConversionUtils
 import utils.ScalazJson4sUtils
 
 /**
-* Companion object. Lets us create an IpLookupsEnrichment
-* instance from a JValue.
-*/
+ * Companion object. Lets us create an IpLookupsEnrichment
+ * instance from a JValue.
+ */
 object IpLookupsEnrichment extends ParseableEnrichment {
 
   val supportedSchema = SchemaCriterion("com.snowplowanalytics.snowplow", "ip_lookups", "jsonschema", 1, 0)
 
   /**
    * Creates an IpLookupsEnrichment instance from a JValue.
-   * 
+   *
    * @param config The ip_lookups enrichment JSON
    * @param schemaKey The SchemaKey provided for the enrichment
    *        Must be a supported SchemaKey for this enrichment
@@ -68,13 +62,11 @@ object IpLookupsEnrichment extends ParseableEnrichment {
    *        Enabled for tests
    * @return a configured IpLookupsEnrichment instance
    */
-  def parse(config: JValue, schemaKey: SchemaKey, localMode: Boolean): ValidatedNelMessage[IpLookupsEnrichment] = {
-
-    isParseable(config, schemaKey).flatMap( conf => {
+  def parse(config: JValue, schemaKey: SchemaKey, localMode: Boolean): ValidatedNelMessage[IpLookupsEnrichment] =
+    isParseable(config, schemaKey).flatMap(conf => {
       def db(name: String) = getArgumentFromName(conf, name).sequenceU
-      (db("geo") |@| db("isp") |@| db("organization") |@| db("domain") |@| db("netspeed")) { IpLookupsEnrichment(_,_,_,_,_,localMode) }
+      (db("geo") |@| db("isp") |@| db("organization") |@| db("domain") |@| db("netspeed")) { IpLookupsEnrichment(_, _, _, _, _, localMode) }
     })
-  }
 
   /**
    * Creates the (URI, String) tuple arguments
@@ -87,8 +79,7 @@ object IpLookupsEnrichment extends ParseableEnrichment {
    *         Some(Failure) if its URI is invalid,
    *         Some(Success) if it is found
    */
-  private def getArgumentFromName(conf: JValue, name: String): Option[ValidatedNelMessage[(String, URI, String)]] = {
-
+  private def getArgumentFromName(conf: JValue, name: String): Option[ValidatedNelMessage[(String, URI, String)]] =
     if (ScalazJson4sUtils.fieldExists(conf, "parameters", name)) {
       val uri = ScalazJson4sUtils.extract[String](conf, "parameters", name, "uri")
       val db  = ScalazJson4sUtils.extract[String](conf, "parameters", name, "database")
@@ -101,7 +92,6 @@ object IpLookupsEnrichment extends ParseableEnrichment {
       }.flatMap(x => x).some
 
     } else None
-  }
 
   /**
    * Convert the Maxmind file from a
@@ -114,10 +104,13 @@ object IpLookupsEnrichment extends ParseableEnrichment {
    * @return a Validation-boxed URI
    */
   private def getMaxmindUri(uri: String, database: String): ValidatedMessage[URI] =
-    ConversionUtils.stringToUri(uri + "/" + database).flatMap(_ match {
-      case Some(u) => u.success
-      case None => "URI to MaxMind file must be provided".fail
-      }).toProcessingMessage
+    ConversionUtils
+      .stringToUri(uri + "/" + database)
+      .flatMap(_ match {
+        case Some(u) => u.success
+        case None    => "URI to MaxMind file must be provided".fail
+      })
+      .toProcessingMessage
 }
 
 /**
@@ -137,29 +130,30 @@ object IpLookupsEnrichment extends ParseableEnrichment {
  * @param netspeedTuple (Full URI to the netspeed
  *        lookup MaxMind data file, database name)
  * @param localMode Whether to use the local
- *        MaxMind data file. Enabled for tests. 
+ *        MaxMind data file. Enabled for tests.
  */
 case class IpLookupsEnrichment(
-  geoTuple: Option[(String, URI, String)],
-  ispTuple: Option[(String, URI, String)],
-  orgTuple: Option[(String, URI, String)],
-  domainTuple: Option[(String, URI, String)],
+  geoTuple: Option[(String,      URI, String)],
+  ispTuple: Option[(String,      URI, String)],
+  orgTuple: Option[(String,      URI, String)],
+  domainTuple: Option[(String,   URI, String)],
   netspeedTuple: Option[(String, URI, String)],
   localMode: Boolean
-  ) extends Enrichment {
+) extends Enrichment {
 
   private type FinalPath = String
-  private type DbEntry = Option[(Option[URI], FinalPath)]
+  private type DbEntry   = Option[(Option[URI], FinalPath)]
 
   // Construct a Tuple5 of all the IP Lookup databases
   private val dbs: Tuple5[DbEntry, DbEntry, DbEntry, DbEntry, DbEntry] = {
 
-    def db(dbPath: Option[(String, URI, String)]): DbEntry = dbPath.map { case (name, uri, file) =>
-      if (localMode) {
-        (None, getClass.getResource(file).toURI.getPath)
-      } else {
-        (Some(uri), "./ip_" + name)
-      }
+    def db(dbPath: Option[(String, URI, String)]): DbEntry = dbPath.map {
+      case (name, uri, file) =>
+        if (localMode) {
+          (None, getClass.getResource(file).toURI.getPath)
+        } else {
+          (Some(uri), "./ip_" + name)
+        }
     }
 
     (db(geoTuple), db(ispTuple), db(orgTuple), db(domainTuple), db(netspeedTuple))
@@ -200,12 +194,10 @@ case class IpLookupsEnrichment(
    *         boxed in a Scalaz Validation
    */
   // TODO: can we move the IpGeo to an implicit?
-  def extractIpInformation(ip: String): Validation[String, IpLookupResult] = {
-
+  def extractIpInformation(ip: String): Validation[String, IpLookupResult] =
     try {
       ipLookups.performLookups(ip).success
     } catch {
       case NonFatal(e) => "Could not extract geo-location from IP address [%s]: [%s]".format(ip, e).fail
     }
-  }
 }
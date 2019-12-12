8legd/snowplow

Stream Enrich: bump scala-common-enrich to 1.0.0 (closes #4148)

3-enrich/stream-enrich/kafka/src/main/scala/com.snowplowanalytics.snowplow.enrich.stream/KafkaEnrich.scala
@@ -16,21 +16,21 @@
  * See the Apache License Version 2.0 for the specific language
  * governing permissions and limitations there under.
  */
-package com.snowplowanalytics
-package snowplow
-package enrich
-package stream
+package com.snowplowanalytics.snowplow.enrich.stream
 
 import java.io.File
 import java.net.URI
 
-import scalaz.Validation
-import common.adapters.AdapterRegistry
-import common.enrichments.EnrichmentRegistry
+import cats.Id
+import com.snowplowanalytics.snowplow.enrich.common.adapters.AdapterRegistry
+import com.snowplowanalytics.snowplow.enrich.common.enrichments.EnrichmentRegistry
+import com.snowplowanalytics.snowplow.enrich.common.outputs.Processor
+import com.snowplowanalytics.iglu.client.Client
+import com.snowplowanalytics.snowplow.scalatracker.Tracker
+import io.circe.Json
+
 import config.FileConfig
-import iglu.client.Resolver
 import model.{Credentials, StreamsConfig}
-import scalatracker.Tracker
 import sources.{KafkaSource, Source}
 
 /** The main entry point for Stream Enrich for Kafka. */
@@ -40,12 +40,13 @@ object KafkaEnrich extends Enrich {
 
   override def getSource(
     streamsConfig: StreamsConfig,
-    resolver: Resolver,
+    client: Client[Id, Json],
     adapterRegistry: AdapterRegistry,
-    enrichmentRegistry: EnrichmentRegistry,
-    tracker: Option[Tracker]
-  ): Validation[String, Source] =
-    KafkaSource.create(streamsConfig, resolver, adapterRegistry, enrichmentRegistry, tracker)
+    enrichmentRegistry: EnrichmentRegistry[Id],
+    tracker: Option[Tracker[Id]],
+    processor: Processor
+  ): Either[String, Source] =
+    KafkaSource.create(streamsConfig, client, enrichmentRegistry, tracker, processor)
 
   override val parser: scopt.OptionParser[FileConfig] = localParser
 
@@ -54,20 +55,20 @@ object KafkaEnrich extends Enrich {
     targetFile: File
   )(
     implicit creds: Credentials
-  ): Validation[String, Int] =
+  ): Either[String, Int] =
     httpDownloader(uri, targetFile)
 
   override def extractResolver(
     resolverArgument: String
   )(
     implicit creds: Credentials
-  ): Validation[String, String] =
+  ): Either[String, String] =
     localResolverExtractor(resolverArgument)
 
   override def extractEnrichmentConfigs(
     enrichmentArg: Option[String]
   )(
     implicit creds: Credentials
-  ): Validation[String, String] =
+  ): Either[String, Json] =
     localEnrichmentConfigsExtractor(enrichmentArg)
 }

snowplow/enrich

modules/kafka/src/main/scala/com.snowplowanalytics.snowplow.enrich.stream/KafkaEnrich.scala
@@ -16,21 +16,21 @@
  * See the Apache License Version 2.0 for the specific language
  * governing permissions and limitations there under.
  */
-package com.snowplowanalytics
-package snowplow
-package enrich
-package stream
+package com.snowplowanalytics.snowplow.enrich.stream
 
 import java.io.File
 import java.net.URI
 
-import scalaz.Validation
-import common.adapters.AdapterRegistry
-import common.enrichments.EnrichmentRegistry
+import cats.Id
+import com.snowplowanalytics.iglu.client.Client
+import com.snowplowanalytics.snowplow.badrows.Processor
+import com.snowplowanalytics.snowplow.enrich.common.adapters.AdapterRegistry
+import com.snowplowanalytics.snowplow.enrich.common.enrichments.EnrichmentRegistry
+import com.snowplowanalytics.snowplow.scalatracker.Tracker
+import io.circe.Json
+
 import config.FileConfig
-import iglu.client.Resolver
 import model.{Credentials, StreamsConfig}
-import scalatracker.Tracker
 import sources.{KafkaSource, Source}
 
 /** The main entry point for Stream Enrich for Kafka. */
@@ -40,12 +40,13 @@ object KafkaEnrich extends Enrich {
 
   override def getSource(
     streamsConfig: StreamsConfig,
-    resolver: Resolver,
+    client: Client[Id, Json],
     adapterRegistry: AdapterRegistry,
-    enrichmentRegistry: EnrichmentRegistry,
-    tracker: Option[Tracker]
-  ): Validation[String, Source] =
-    KafkaSource.create(streamsConfig, resolver, adapterRegistry, enrichmentRegistry, tracker)
+    enrichmentRegistry: EnrichmentRegistry[Id],
+    tracker: Option[Tracker[Id]],
+    processor: Processor
+  ): Either[String, Source] =
+    KafkaSource.create(streamsConfig, client, adapterRegistry, enrichmentRegistry, processor)
 
   override val parser: scopt.OptionParser[FileConfig] = localParser
 
@@ -54,20 +55,20 @@ object KafkaEnrich extends Enrich {
     targetFile: File
   )(
     implicit creds: Credentials
-  ): Validation[String, Int] =
+  ): Either[String, Int] =
     httpDownloader(uri, targetFile)
 
   override def extractResolver(
     resolverArgument: String
   )(
     implicit creds: Credentials
-  ): Validation[String, String] =
+  ): Either[String, String] =
     localResolverExtractor(resolverArgument)
 
   override def extractEnrichmentConfigs(
     enrichmentArg: Option[String]
   )(
     implicit creds: Credentials
-  ): Validation[String, String] =
+  ): Either[String, Json] =
     localEnrichmentConfigsExtractor(enrichmentArg)
 }
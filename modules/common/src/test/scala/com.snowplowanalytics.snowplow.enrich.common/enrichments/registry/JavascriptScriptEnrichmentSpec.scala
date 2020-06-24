49382b90020b26c6fea57d2bf26743db1baa4616
Benjamin Benoist <benjamin.benoist@snowplowanalytics.com>
2020-06-24 10:40:59 +0200

    Common: use Nashorn instead of Rhino for JS enrichment (close #138)

--- modules/common/src/test/scala/com.snowplowanalytics.snowplow.enrich.common/enrichments/registry/JavascriptScriptEnrichmentSpec.scala
+++ modules/common/src/test/scala/com.snowplowanalytics.snowplow.enrich.common/enrichments/registry/JavascriptScriptEnrichmentSpec.scala
@@ -11,75 +11,141 @@
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
 package com.snowplowanalytics.snowplow.enrich.common
+
 package enrichments.registry
 
 import io.circe.literal._
 
+import org.specs2.Specification
+
 import com.snowplowanalytics.iglu.core.{SchemaKey, SchemaVer, SelfDescribingData}
 
-import org.specs2.Specification
+import com.snowplowanalytics.snowplow.badrows.FailureDetails
 
 import outputs.EnrichedEvent
 
-/** Tests the anonymzeIp function */
 class JavascriptScriptEnrichmentSpec extends Specification {
   def is = s2"""
-  compiling an invalid JavaScript script should fail              $e1
-  a JavaScript script should be able to throw an exception safely $e2
-  a JavaScript script should successfully generate a new context  $e3
+  Javascript enrichment should fail if the function isn't valid                     $e1
+  Javascript enrichment should fail if the function doesn't return an array         $e2
+  Javascript enrichment should fail if the function doesn't return an array of SDJs $e3
+  Javascript enrichment should be able to access the fields of the enriched event   $e4
+  Javascript enrichment should be able to update the fields of the enriched event   $e5
+  Javascript enrichment should be able to throw an exception                        $e6
+  Javascript enrichment should be able to return no new context                     $e7
+  Javascript enrichment should be able to return 2 new contexts                     $e8
   """
 
-  val PreparedEnrichment = {
-    val script =
-      s"""|function process(event) {
-          |  var platform = event.getPlatform(),
-          |      appId    = event.getApp_id();
-          |
-          |  if (platform == "server" && appId != "secret") {
-          |    throw "Server-side event has invalid app_id: " + appId;
-          |  }
-          |
-          |  // Use new String() because http://nelsonwells.net/2012/02/json-stringify-with-mapped-variables/
-          |  var appIdUpper = new String(appId.toUpperCase());
-          |  return [ { schema: "iglu:com.acme/foo/jsonschema/1-0-0",
-          |             data:   { appIdUpper: appIdUpper }
-          |           } ];
-          |}
-          |""".stripMargin
+  val schemaKey =
+    SchemaKey("com.snowplowanalytics.snowplow", "javascript_script_config", "jsonschema", SchemaVer.Full(1, 0, 0))
 
-    val compiled = JavascriptScriptEnrichment.compile(script)
-    val schemaKey = SchemaKey("vendor", "name", "format", SchemaVer.Full(1, 0, 0))
-    JavascriptScriptEnrichment(schemaKey, compiled.right.get)
+  def e1 =
+    JavascriptScriptEnrichment(schemaKey, "[").process(buildEnriched()) must beLeft(
+      failureContains(_: FailureDetails.EnrichmentFailure, "Error compiling")
+    )
+
+  def e2 = {
+    val function = s"""
+      function process(event) {
+        return { foo: "bar" }
+      }"""
+    JavascriptScriptEnrichment(schemaKey, function).process(buildEnriched()) must beLeft(
+      failureContains(_: FailureDetails.EnrichmentFailure, "not read as an array")
+    )
   }
 
-  def buildEvent(appId: String): EnrichedEvent = {
+  def e3 = {
+    val function = s"""
+      function process(event) {
+        return [ { foo: "bar" } ]
+      }"""
+    JavascriptScriptEnrichment(schemaKey, function).process(buildEnriched()) must beLeft(
+      failureContains(_: FailureDetails.EnrichmentFailure, "not self-desribing")
+    )
+  }
+
+  def e4 = {
+    val appId = "greatApp"
+    val function = s"""
+      function process(event) {
+        return [ { schema: "iglu:com.acme/foo/jsonschema/1-0-0",
+          data:   { appId: event.getApp_id() }
+        } ];
+      }"""
+    JavascriptScriptEnrichment(schemaKey, function).process(buildEnriched(appId)) must beRight.like {
+      case List(sdj) if sdj.data.noSpaces.contains(appId) => true
+      case _ => false
+    }
+  }
+
+  def e5 = {
+    val appId = "greatApp"
+    val enriched = buildEnriched(appId)
+    val newAppId = "evenBetterApp"
+    val function = s"""
+      function process(event) {
+        event.setApp_id("$newAppId")
+        return [ { schema: "iglu:com.acme/foo/jsonschema/1-0-0",
+          data:   { foo: "bar" }
+        } ];
+      }"""
+    JavascriptScriptEnrichment(schemaKey, function).process(enriched)
+    enriched.app_id must beEqualTo(newAppId)
+  }
+
+  def e6 = {
+    val function = s"""
+      function process(event) {
+        throw "Error"
+      }"""
+    JavascriptScriptEnrichment(schemaKey, function).process(buildEnriched()) must beLeft(
+      failureContains(_: FailureDetails.EnrichmentFailure, "Error during execution")
+    )
+  }
+
+  def e7 = {
+    val function = s"""
+      function process(event) {
+        return [ ];
+      }"""
+    JavascriptScriptEnrichment(schemaKey, function).process(buildEnriched()) must beRight
+  }
+
+  def e8 = {
+    val function = s"""
+      function process(event) {
+        return [ { schema: "iglu:com.acme/foo/jsonschema/1-0-0",
+          data:   { hello: "world" }
+        }, { schema: "iglu:com.acme/bar/jsonschema/1-0-0",
+          data:   { hello: "world" }
+        } ];
+      }"""
+
+    val context1 =
+      SelfDescribingData(
+        SchemaKey("com.acme", "foo", "jsonschema", SchemaVer.Full(1, 0, 0)),
+        json"""{"hello":"world"}"""
+      )
+    val context2 =
+      SelfDescribingData(
+        SchemaKey("com.acme", "bar", "jsonschema", SchemaVer.Full(1, 0, 0)),
+        json"""{"hello":"world"}"""
+      )
+    JavascriptScriptEnrichment(schemaKey, function).process(buildEnriched()) must beRight.like {
+      case List(c1, c2) if c1 == context1 && c2 == context2 => true
+      case _ => false
+    }
+  }
+
+  def buildEnriched(appId: String = "my super app"): EnrichedEvent = {
     val e = new EnrichedEvent()
     e.platform = "server"
     e.app_id = appId
     e
   }
 
-  def e1 = {
-    val actual = JavascriptScriptEnrichment.compile("[")
-    actual must beLeft
+  def failureContains(failure: FailureDetails.EnrichmentFailure, pattern: String): Boolean = failure match {
+    case FailureDetails.EnrichmentFailure(_, FailureDetails.EnrichmentFailureMessage.Simple(msg)) if msg.contains(pattern) => true
+    case _ => false
   }
-
-  def e2 = {
-    val event = buildEvent("guess")
-    val actual = PreparedEnrichment.process(event)
-    actual must beLeft
-  }
-
-  def e3 = {
-    val event = buildEvent("secret")
-    val actual = PreparedEnrichment.process(event)
-
-    val expected =
-      SelfDescribingData(
-        SchemaKey("com.acme", "foo", "jsonschema", SchemaVer.Full(1, 0, 0)),
-        json"""{"appIdUpper":"SECRET"}"""
-      )
-    actual must beRight.like { case head :: Nil => head must_== expected }
-  }
-
 }

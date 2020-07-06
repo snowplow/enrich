05949bf9aec26b6ed8ead2794ce75b3314fc4610
Oguzhan Unlu <oguzhan@snowplowanalytics.com>
2020-07-06 12:24:15 +0300

    bump to rc10

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -36,7 +36,7 @@ object BuildSettings {
   lazy val basicSettings = Seq(
     organization          :=  "com.snowplowanalytics",
     scalaVersion          :=  "2.12.11",
-    version               :=  "1.3.0-rc9",
+    version               :=  "1.3.0-rc10",
     javacOptions          :=  Seq("-source", "1.8", "-target", "1.8"),
     resolvers             ++= Dependencies.resolutionRepos
   )

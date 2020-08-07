ab815fe333ebee19478027b509801720e9f76741
Anton Parkhomenko <mailbox@chuwy.me>
2020-08-07 23:53:46 +0300

    Common: add toThrift and toRaw methods to CollectorPayload (close #345)

--- modules/common/src/test/scala/com.snowplowanalytics.snowplow.enrich.common/loaders/CollectorPayloadSpec.scala
+++ modules/common/src/test/scala/com.snowplowanalytics.snowplow.enrich.common/loaders/CollectorPayloadSpec.scala
@@ -12,10 +12,23 @@
  */
 package com.snowplowanalytics.snowplow.enrich.common.loaders
 
-import org.specs2.mutable.Specification
-import org.specs2.matcher.DataTables
+import cats.syntax.option._
 
-class CollectorPayloadSpec extends Specification with DataTables {
+import org.apache.http.NameValuePair
+import org.apache.http.message.BasicNameValuePair
+import org.apache.thrift.TSerializer
+
+import org.joda.time.{DateTimeZone, LocalDate}
+
+import org.scalacheck.{Arbitrary, Gen}
+
+import org.specs2.ScalaCheck
+import org.specs2.mutable.Specification
+import org.specs2.matcher.{DataTables, ValidatedMatchers}
+
+import com.snowplowanalytics.snowplow.badrows.Processor
+
+class CollectorPayloadSpec extends Specification with DataTables with ScalaCheck with ValidatedMatchers {
 
   // TODO: let's abstract this up to a CollectorApi.parse test
   // (then we can make isIceRequest private again).
@@ -31,4 +44,74 @@ class CollectorPayloadSpec extends Specification with DataTables {
       }
     }
   }
+
+  "toThrift" should {
+    implicit val arbitraryPayload: Arbitrary[CollectorPayload] =
+      Arbitrary(CollectorPayloadSpec.collectorPayloadGen)
+
+    "be isomorphic to ThriftLoader.toCollectorPayload" >> {
+      prop { payload: CollectorPayload =>
+        val bytes = CollectorPayloadSpec.thriftSerializer.serialize(payload.toThrift)
+        val result = ThriftLoader.toCollectorPayload(bytes, Processor("test", "0.0.1"))
+        result must beValid(Some(payload))
+      }
+    }
+  }
+}
+
+object CollectorPayloadSpec {
+
+  val thriftSerializer = new TSerializer()
+
+  val apiGen = Gen.oneOf(
+    CollectorPayload.Api("com.snowplowanalytics.snowplow", "tp1"),
+    CollectorPayload.Api("com.snowplowanalytics.snowplow", "tp2"),
+    CollectorPayload.Api("r", "tp2"),
+    CollectorPayload.Api("com.snowplowanalytics.iglu", "v1"),
+    CollectorPayload.Api("com.mailchimp", "v1")
+  )
+
+  val nameValuePair = for {
+    k <- Gen.oneOf("qkey", "key2", "key_3", "key-4", "key 5")
+    v <- Gen.option(Gen.oneOf("iglu:com.acme/under_score/jsonschema/1-0-3", "foo", "1", "null"))
+  } yield new BasicNameValuePair(k, v.orNull)
+  val queryParametersGen: Gen[List[NameValuePair]] =
+    for {
+      n <- Gen.chooseNum(0, 4)
+      list <- Gen.listOfN[NameValuePair](n, nameValuePair)
+    } yield list
+
+  val contentTypeGen: Gen[String] = Gen.oneOf("text/plain", "application/json", "application/json; encoding=utf-8")
+
+  val source: CollectorPayload.Source = CollectorPayload.Source("host", "UTF-8", "localhost".some)
+
+  val localDateGen: Gen[LocalDate] = Gen.calendar.map(LocalDate.fromCalendarFields)
+  val ipGen: Gen[String] = for {
+    part1 <- Gen.choose(2, 255)
+    part2 <- Gen.choose(0, 255)
+    part3 <- Gen.choose(0, 255)
+    part4 <- Gen.choose(0, 255)
+  } yield s"$part1.$part2.$part3.$part4"
+  val headerGen: Gen[String] = for {
+    first <- Gen.asciiPrintableStr.map(_.capitalize)
+    second <- Gen.option(Gen.asciiPrintableStr.map(_.capitalize))
+    key = second.fold(first)(s => s"$first-$s")
+    value <- Gen.identifier
+  } yield s"$key: $value"
+  val contextGen: Gen[CollectorPayload.Context] = for {
+    timestamp <- localDateGen.map(_.toDateTimeAtStartOfDay(DateTimeZone.UTC)).map(Option.apply)
+    ip <- Gen.option(ipGen)
+    userAgent <- Gen.option(Gen.identifier)
+    headersN <- Gen.chooseNum(0, 8)
+    headers <- Gen.listOfN(headersN, headerGen)
+    userId <- Gen.option(Gen.uuid)
+  } yield CollectorPayload.Context(timestamp, ip, userAgent, None, headers, userId)
+
+  val collectorPayloadGen: Gen[CollectorPayload] = for {
+    api <- apiGen
+    kvlist <- queryParametersGen
+    contentType <- Gen.option(contentTypeGen)
+    body <- Gen.option(Gen.asciiPrintableStr.suchThat(_.nonEmpty))
+    context <- contextGen
+  } yield CollectorPayload(api, kvlist, contentType, body, source, context)
 }

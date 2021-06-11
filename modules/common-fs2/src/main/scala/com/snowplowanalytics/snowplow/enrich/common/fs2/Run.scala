140892854692d9cb0dc470f63474a9fe099ef760
Benjamin Benoist <benjamin.benoist@snowplowanalytics.com>
2021-06-11 17:31:27 +0200

    Stream FS2: split into common module and PubSub module (close #473)

---
+++ modules/common-fs2/src/main/scala/com/snowplowanalytics/snowplow/enrich/common/fs2/Run.scala
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2021-2021 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.snowplow.enrich.common.fs2
+
+import cats.Parallel
+import cats.implicits._
+
+import _root_.io.sentry.SentryClient
+
+import fs2.{Pipe, Stream}
+
+import scala.concurrent.ExecutionContext
+
+import cats.effect.{Blocker, Clock, Concurrent, ContextShift, ConcurrentEffect, ExitCode, Resource, Sync, Timer}
+
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
+
+import com.snowplowanalytics.snowplow.badrows.Processor
+
+import com.snowplowanalytics.snowplow.enrich.common.fs2.config.io.{Authentication, Input, Output}
+import com.snowplowanalytics.snowplow.enrich.common.fs2.config.{CliConfig, ParsedConfigs}
+import com.snowplowanalytics.snowplow.enrich.common.fs2.io.{Sink, Source}
+
+object Run {
+  private implicit def unsafeLogger[F[_]: Sync]: Logger[F] =
+    Slf4jLogger.getLogger[F]
+
+  def run[F[_]: Clock: ConcurrentEffect: ContextShift: Parallel: Timer, A](
+    args: List[String],
+    name: String,
+    version: String,
+    description: String,
+    ec: ExecutionContext,
+    mkSource: (Blocker, Authentication, Input) => Stream[F, A],
+    mkGoodSink: (Blocker, Authentication, Output) => Resource[F, AttributedByteSink[F]],
+    mkPiiSink: (Blocker, Authentication, Output) => Resource[F, AttributedByteSink[F]],
+    mkBadSink: (Blocker, Authentication, Output) => Resource[F, ByteSink[F]],
+    checkpointer: Pipe[F, A, Unit],
+    getPayload: A => Array[Byte],
+    ordered: Boolean
+  ): F[ExitCode] = 
+    CliConfig.command(name, version, description).parse(args) match {
+      case Right(cfg) =>
+
+        ParsedConfigs.parse[F](cfg).fold (
+          err => Sync[F].delay(System.err.println(err)).as[ExitCode](ExitCode.Error),
+          parsed => 
+            Blocker[F].use { blocker =>
+              for {
+                _ <- Logger[F].info(s"Initialising resources for $name $version")
+                processor = Processor(name, version)
+                file = parsed.configFile
+                goodSink = initAttributedSink(blocker, file.auth, file.good, mkGoodSink)
+                piiSink = file.pii.map(out => initAttributedSink(blocker, file.auth, out, mkPiiSink))
+                badSink = file.bad match {
+                  case Output.FileSystem(path) =>
+                    Sink.fileSink[F](path, blocker)
+                  case _ =>
+                    mkBadSink(blocker, file.auth, file.bad)
+                }
+                exit <- 
+                  (file.auth, file.input) match {
+                    case (_, p: Input.FileSystem) => 
+                      val env = Environment
+                        .make[F, Array[Byte]](
+                          blocker,
+                          ec,
+                          parsed,
+                          Source.filesystem[F](blocker, p.dir),
+                          goodSink,
+                          piiSink,
+                          badSink,
+                          _.void,
+                          identity,
+                          processor
+                        )
+                      runEnvironment[F, Array[Byte]](env, false)
+                    case _ =>
+                      val env = Environment
+                        .make[F, A](
+                          blocker,
+                          ec,
+                          parsed,
+                          mkSource(blocker, file.auth, file.input),
+                          goodSink,
+                          piiSink,
+                          badSink,
+                          checkpointer,
+                          getPayload,
+                          processor
+                        )
+                      runEnvironment[F, A](env, ordered)
+                  }
+              } yield exit
+            }
+        ).flatten
+      case Left(error) =>
+        Sync[F].delay(System.err.println(error)) >> Sync[F].pure(ExitCode.Error)
+    }
+
+  private def initAttributedSink[F[_]: Concurrent: ContextShift: Timer](
+    blocker: Blocker,
+    auth: Authentication,
+    output: Output,
+    mkGoodSink: (Blocker, Authentication, Output) => Resource[F, AttributedByteSink[F]],
+  ): Resource[F, AttributedByteSink[F]] =
+    output match {
+      case Output.FileSystem(path) =>
+        Sink.fileSink[F](path, blocker).map(sink => row => sink(row.data))
+      case _ =>
+        mkGoodSink(blocker, auth, output)
+    }
+
+  private def runEnvironment[F[_]: ConcurrentEffect: ContextShift: Parallel: Timer, A](
+    environment: Resource[F, Environment[F, A]],
+    ordered: Boolean
+  ): F[ExitCode] =
+    environment.use { env =>
+      val log = Logger[F].info("Running enrichment stream")
+      val enrich = Enrich.run[F, A](env, ordered)
+      val updates = Assets.run[F, A](env)
+      val reporting = env.metrics.report
+      val flow = enrich.merge(updates).merge(reporting)
+      log >> flow.compile.drain.attempt.flatMap {
+        case Left(exception) =>
+          sendToSentry(exception, env.sentry) >>
+          Logger[F].error(s"The Enrich job has stopped") >>
+          Sync[F].raiseError[ExitCode](exception)
+        case Right(_) =>
+          Sync[F].pure(ExitCode.Success)
+      }
+    }
+
+  private def sendToSentry[F[_]: Sync](error: Throwable, sentry: Option[SentryClient]): F[Unit] =
+    sentry match {
+      case Some(client) =>
+        Sync[F].delay(client.sendException(error)) >> Logger[F].info("Sentry report has been sent")
+      case None =>
+        Sync[F].unit
+    }
+  }
---
+++ modules/common-fs2/src/test/scala/com/snowplowanalytics/snowplow/enrich/common/fs2/enrichments/IabEnrichmentSpec.scala
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2020-2021 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.snowplow.enrich.common.fs2.enrichments
+
+import java.net.URI
+
+import scala.concurrent.duration._
+
+import cats.syntax.apply._
+import cats.syntax.option._
+
+import cats.effect.testing.specs2.CatsIO
+
+import io.circe.literal._
+
+import fs2.Stream
+
+import org.specs2.mutable.Specification
+
+import com.snowplowanalytics.iglu.core.{SchemaKey, SchemaVer, SelfDescribingData}
+
+import com.snowplowanalytics.snowplow.analytics.scalasdk.SnowplowEvent.Contexts
+
+import com.snowplowanalytics.snowplow.enrich.common.enrichments.registry.EnrichmentConf
+
+import com.snowplowanalytics.snowplow.enrich.common.fs2.EnrichSpec
+import com.snowplowanalytics.snowplow.enrich.common.fs2.test.{HttpServer, TestEnvironment}
+
+class IabEnrichmentSpec extends Specification with CatsIO {
+
+  sequential
+
+  "IabEnrichment" should {
+    "recognize a robot by IP address" in {
+      val payload = EnrichSpec.collectorPayload.copy(
+        context = EnrichSpec.collectorPayload.context.copy(
+          useragent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0".some
+        )
+      )
+      val input = Stream(payload.toRaw)
+      val expected = Contexts(
+        List(
+          SelfDescribingData(
+            SchemaKey("com.iab.snowplow", "spiders_and_robots", "jsonschema", SchemaVer.Full(1, 0, 0)),
+            json"""{"spiderOrRobot":true,"category":"SPIDER_OR_ROBOT","reason":"FAILED_IP_EXCLUDE","primaryImpact":"UNKNOWN"}"""
+          )
+        )
+      )
+      val testWithHttp = HttpServer.resource(6.seconds) *> TestEnvironment.make(input, List(IabEnrichmentSpec.enrichmentConf))
+      testWithHttp.use { test =>
+        test.run().map {
+          case (bad, pii, good) =>
+            (bad must be empty)
+            (pii must be empty)
+            good.map(_.derived_contexts) must contain(exactly(expected))
+        }
+      }
+    }
+
+    "refresh assets" in {
+      val payload = EnrichSpec.collectorPayload.copy(
+        context = EnrichSpec.collectorPayload.context.copy(
+          useragent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0".some
+        )
+      )
+      val input = Stream(payload.toRaw) ++ Stream.sleep_(2.seconds) ++ Stream(payload.toRaw)
+
+      val expectedOne = Contexts(
+        List(
+          SelfDescribingData(
+            SchemaKey("com.iab.snowplow", "spiders_and_robots", "jsonschema", SchemaVer.Full(1, 0, 0)),
+            json"""{"spiderOrRobot":true,"category":"SPIDER_OR_ROBOT","reason":"FAILED_IP_EXCLUDE","primaryImpact":"UNKNOWN"}"""
+          )
+        )
+      )
+      val expectedTwo = Contexts(
+        List(
+          SelfDescribingData(
+            SchemaKey("com.iab.snowplow", "spiders_and_robots", "jsonschema", SchemaVer.Full(1, 0, 0)),
+            json"""{"spiderOrRobot":false,"category":"BROWSER","reason":"PASSED_ALL","primaryImpact":"NONE"}"""
+          )
+        )
+      )
+
+      val testWithHttp = HttpServer.resource(6.seconds) *> TestEnvironment.make(input, List(IabEnrichmentSpec.enrichmentConf))
+      testWithHttp.use { test =>
+        test.run(_.copy(assetsUpdatePeriod = Some(1800.millis))).map {
+          case (bad, pii, good) =>
+            (bad must be empty)
+            (pii must be empty)
+            good.map(_.derived_contexts) must contain(exactly(expectedOne, expectedTwo))
+        }
+      }
+    }
+  }
+}
+
+object IabEnrichmentSpec {
+  val enrichmentConf = EnrichmentConf.IabConf(
+    SchemaKey("com.acme", "enrichment", "jsonschema", SchemaVer.Full(1, 0, 0)),
+    (URI.create("http://localhost:8080/iab/ip"), "ip"),
+    (URI.create("http://localhost:8080/iab/exclude"), "exclude"),
+    (URI.create("http://localhost:8080/iab/include"), "include")
+  )
+}

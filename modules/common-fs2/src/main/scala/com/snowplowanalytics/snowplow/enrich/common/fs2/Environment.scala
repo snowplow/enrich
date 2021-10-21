7d08a787fe28315b56586ba0250937d3b69caaef
Benjamin Benoist <benjamin.benoist@snowplowanalytics.com>
2021-10-21 09:48:32 +0200

    Environment: getRegion

--- modules/common-fs2/src/main/scala/com/snowplowanalytics/snowplow/enrich/common/fs2/Environment.scala
+++ modules/common-fs2/src/main/scala/com/snowplowanalytics/snowplow/enrich/common/fs2/Environment.scala
@@ -226,7 +226,7 @@ object Environment {
 
   private def getRegionFromConfig(file: ConfigFile): Option[String] =
     file.input match {
-      case Kinesis(_, _, region, _, _, _) =>
+      case Kinesis(_, _, region, _, _, _, _, _) =>
         region
       case _ =>
         None

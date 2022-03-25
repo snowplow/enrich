56a5f86faa8a6f1c07a535243fb04976fc31734e
Ian Streeter <ian@snowplowanalytics.com>
2022-03-25 12:04:54 +0000

    enrich-kinesis: Harden fetching enrichment configs from DynamoDB (close #578)

--- modules/kinesis/src/main/scala/com/snowplowanalytics/snowplow/enrich/kinesis/DynamoDbConfig.scala
+++ modules/kinesis/src/main/scala/com/snowplowanalytics/snowplow/enrich/kinesis/DynamoDbConfig.scala
@@ -12,8 +12,9 @@
  */
 package com.snowplowanalytics.snowplow.enrich.kinesis
 
-import cats.effect.{Blocker, ContextShift, Sync}
+import cats.effect.{Blocker, ContextShift, Resource, Sync, Timer}
 
+import cats.Applicative
 import cats.implicits._
 
 import io.circe.parser._
@@ -23,11 +24,16 @@ import io.circe.Json
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 
-import scala.collection.JavaConverters._
+import retry.{RetryDetails, RetryPolicies, RetryPolicy, retryingOnSomeErrors}
 
+import scala.collection.JavaConverters._
+import scala.concurrent.duration.DurationLong
+import scala.util.control.NonFatal
+
+import com.amazonaws.AmazonClientException
 import com.amazonaws.services.dynamodbv2.model.ScanRequest
 import com.amazonaws.services.dynamodbv2.document.DynamoDB
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+import com.amazonaws.services.dynamodbv2.{AmazonDynamoDB, AmazonDynamoDBClientBuilder}
 
 import com.snowplowanalytics.iglu.core.{SchemaKey, SchemaVer, SelfDescribingData}
 import com.snowplowanalytics.iglu.core.circe.CirceIgluCodecs._
@@ -52,14 +58,14 @@ object DynamoDbConfig {
    * Retrieves JSONs from DynamoDB if cli arguments start with dynamodb:
    *  and passes them as base64 encoded JSONs
    */
-  def updateCliConfig[F[_]: ContextShift: Sync](blocker: Blocker, original: CliConfig): F[CliConfig] =
+  def updateCliConfig[F[_]: ContextShift: Sync: Timer](blocker: Blocker, original: CliConfig): F[CliConfig] =
     for {
       resolver <- updateArg[F](blocker, original.resolver, getResolver[F])
       enrichments <- updateArg[F](blocker, original.enrichments, getEnrichments[F])
       updated = original.copy(resolver = resolver, enrichments = enrichments)
     } yield updated
 
-  private def updateArg[F[_]: ContextShift: Sync](
+  private def updateArg[F[_]: ContextShift: Sync: Timer](
     blocker: Blocker,
     orig: EncodedOrPath,
     getConfig: (Blocker, String, String, String) => F[Base64Json]
@@ -73,64 +79,92 @@ object DynamoDbConfig {
         }
     }
 
-  private def getResolver[F[_]: ContextShift: Sync](
+  private def getResolver[F[_]: ContextShift: Sync: Timer](
     blocker: Blocker,
     region: String,
     table: String,
     key: String
   ): F[Base64Json] = {
-    val dynamoDBClient = mkClient(region)
-    val dynamoDB = new DynamoDB(dynamoDBClient)
-    for {
-      _ <- unsafeLogger.info(s"Retrieving resolver in DynamoDB $region/$table/$key")
-      item <- blocker.blockOn(Sync[F].delay(dynamoDB.getTable(table).getItem("id", key)))
-      jsonStr <- Option(item).flatMap(i => Option(i.getString("json"))) match {
-                   case Some(content) =>
-                     Sync[F].pure(content)
-                   case None =>
-                     Sync[F].raiseError[String](new RuntimeException(s"Can't retrieve resolver in DynamoDB at $region/$table/$key"))
-                 }
-      json <- parse(jsonStr) match {
-                case Right(parsed) =>
-                  Sync[F].pure(parsed)
-                case Left(err) =>
-                  Sync[F].raiseError[Json](new RuntimeException(s"Can't parse resolver. Error: $err"))
-              }
-      encoded = Base64Json(json)
-    } yield encoded
+    val dynamoDBResource = for {
+      client <- mkClient[F](region)
+      api <- Resource.make(Sync[F].delay(new DynamoDB(client)))(a => Sync[F].delay(a.shutdown()))
+    } yield api
+    dynamoDBResource.use { dynamoDB =>
+      for {
+        _ <- unsafeLogger.info(s"Retrieving resolver in DynamoDB $region/$table/$key")
+        item <- withRetry(blocker.blockOn(Sync[F].delay(dynamoDB.getTable(table).getItem("id", key))))
+        jsonStr <- Option(item).flatMap(i => Option(i.getString("json"))) match {
+                     case Some(content) =>
+                       Sync[F].pure(content)
+                     case None =>
+                       Sync[F].raiseError[String](new RuntimeException(s"Can't retrieve resolver in DynamoDB at $region/$table/$key"))
+                   }
+        json <- parse(jsonStr) match {
+                  case Right(parsed) =>
+                    Sync[F].pure(parsed)
+                  case Left(err) =>
+                    Sync[F].raiseError[Json](new RuntimeException(s"Can't parse resolver. Error: $err"))
+                }
+        encoded = Base64Json(json)
+      } yield encoded
+    }
   }
 
-  private def getEnrichments[F[_]: ContextShift: Sync](
+  private def getEnrichments[F[_]: ContextShift: Sync: Timer](
     blocker: Blocker,
     region: String,
     table: String,
     prefix: String
-  ): F[Base64Json] = {
-    val dynamoDBClient = mkClient(region)
-    // Assumes that the table is small and contains only the config
-    val scanRequest = new ScanRequest().withTableName(table)
-    for {
-      _ <- unsafeLogger.info(s"Retrieving enrichments in DynamoDB $region/$table/$prefix*")
-      scanned <- blocker.blockOn(Sync[F].delay(dynamoDBClient.scan(scanRequest)))
-      values = scanned.getItems().asScala.collect {
-                 case map if Option(map.get("id")).exists(_.getS.startsWith(prefix)) && map.containsKey("json") =>
-                   map.get("json").getS()
-               }
-      jsons <- values.map(parse).toList.sequence match {
-                 case Left(decodingFailure) =>
-                   val msg = s"An error occured while parsing an enrichment config: ${decodingFailure.message}"
-                   Sync[F].raiseError(new RuntimeException(msg))
-                 case Right(parsed) =>
-                   Sync[F].pure(parsed)
-               }
-      sdj = SelfDescribingData[Json](enrichmentSchema, Json.arr(jsons: _*))
-      encoded = Base64Json(sdj.asJson)
-    } yield encoded
-  }
+  ): F[Base64Json] =
+    mkClient[F](region).use { dynamoDBClient =>
+      // Assumes that the table is small and contains only the config
+      val scanRequest = new ScanRequest().withTableName(table)
+      for {
+        _ <- unsafeLogger.info(s"Retrieving enrichments in DynamoDB $region/$table/$prefix*")
+        scanned <- withRetry(blocker.blockOn(Sync[F].delay(dynamoDBClient.scan(scanRequest))))
+        values = scanned.getItems().asScala.collect {
+                   case map if Option(map.get("id")).exists(_.getS.startsWith(prefix)) && map.containsKey("json") =>
+                     map.get("json").getS()
+                 }
+        jsons <- values.map(parse).toList.sequence match {
+                   case Left(decodingFailure) =>
+                     val msg = s"An error occured while parsing an enrichment config: ${decodingFailure.message}"
+                     Sync[F].raiseError(new RuntimeException(msg))
+                   case Right(parsed) =>
+                     Sync[F].pure(parsed)
+                 }
+        sdj = SelfDescribingData[Json](enrichmentSchema, Json.arr(jsons: _*))
+        encoded = Base64Json(sdj.asJson)
+      } yield encoded
+    }
 
-  private def mkClient(region: String) =
-    AmazonDynamoDBClientBuilder
-      .standard()
-      .withRegion(region)
-      .build()
+  private def mkClient[F[_]: Sync](region: String): Resource[F, AmazonDynamoDB] =
+    Resource.make(Sync[F].delay {
+      AmazonDynamoDBClientBuilder
+        .standard()
+        .withRegion(region)
+        .build()
+    })(c => Sync[F].delay(c.shutdown))
+
+  private def withRetry[F[_]: Sync: Timer, A](f: F[A]): F[A] =
+    retryingOnSomeErrors[A](retryPolicy[F], worthRetrying, onError[F])(f)
+
+  private def retryPolicy[F[_]: Applicative]: RetryPolicy[F] =
+    RetryPolicies.fullJitter[F](1500.milliseconds).join(RetryPolicies.limitRetries[F](5))
+
+  private def worthRetrying(e: Throwable): Boolean =
+    e match {
+      case ace: AmazonClientException if ace.isRetryable => true
+      case NonFatal(_) => false
+    }
+
+  private def onError[F[_]: Sync](error: Throwable, details: RetryDetails): F[Unit] =
+    if (details.givingUp)
+      Logger[F].error(show"Failed to query dynamodb after ${details.retriesSoFar} retries. ${error.getMessage}. Aborting the job")
+    else
+      Logger[F].warn(
+        show"Failed to query dynamodb with ${details.retriesSoFar} retries so far, " +
+          show"waiting for ${details.cumulativeDelay.toMillis} ms. ${error.getMessage}. " +
+          show"Keep retrying"
+      )
 }

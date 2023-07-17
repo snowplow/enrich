8b2e645368443132422bfa910587482a36216b55
Benjamin Benoist <benjamin.benoist@snowplowanalytics.com>
2023-07-17 19:38:38 +0200

    Bump uap-java to 1.5.4 (close #802)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -34,7 +34,7 @@ object Dependencies {
     val commonsText      = "1.10.0"
     val jodaTime         = "2.10.1"
     val useragent        = "1.21"
-    val uaParser         = "1.4.3"
+    val uaParser         = "1.5.4"
     val snakeYaml        = "1.31" // override transitive dependency to mitigate security vulnerabilities CVE-2022-25857
     val postgresDriver   = "42.5.2"
     val mysqlConnector   = "8.0.29"

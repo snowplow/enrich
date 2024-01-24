8645bafa28f3833bd74a6988e32849f4358e8ddf
Benjamin Benoist <benjamin.benoist@snowplowanalytics.com>
2024-01-24 16:31:38 +0100

    Bump AWS SDK to 1.12.643 (#857)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -67,7 +67,7 @@ object Dependencies {
     val collectorPayload = "0.0.0"
     val schemaSniffer    = "0.0.0"
 
-    val awsSdk           = "1.12.506"
+    val awsSdk           = "1.12.643"
     val gcpSdk           = "2.14.0"
     val awsSdk2          = "2.18.7"
     val kinesisClient2   = "2.4.3"
